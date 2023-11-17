aa4c9022e790067e9820652c25f08efa996b17a6
khanhnehe <103097409+khanhnehe@users.noreply.github.com>
2023-11-17 20:58:52 +0700

    79.91

--- src/containers/System/Admin/ManageStaff.js
+++ src/containers/System/Admin/ManageStaff.js
@@ -6,7 +6,7 @@ import MdEditor from 'react-markdown-editor-lite';
 import 'react-markdown-editor-lite/lib/index.css';
 import './ManageStaff.scss'
 import Select from 'react-select';
-import { LANGUAGES } from "../../../utils"
+import { CRUD_ACTIONS, LANGUAGES } from "../../../utils"
 import { getDetailInforDoctor } from '../../../services/userService';
 
 // const options = [
@@ -31,6 +31,8 @@ class ManageStaff extends Component {
             //78 save to staff_infor table
             listPrice: [],
             listPayment: [],
+            selectedPrice: '',
+            selectedPayment: '',
             note: ''
 
         };
@@ -48,14 +50,37 @@ class ManageStaff extends Component {
         let result = [];
         let language = this.props.language;
         if (inputData && inputData.length > 0) {
-            inputData.map((item, index) => {
-                let object = {};
-                let labelVI = type === 'USERS' ? `${item.lastName} ${item.firstName}` : item.valueVI;
-                let labelEN = type === 'USERS' ? `${item.lastName} ${item.firstName}` : item.valueEN;
-                object.label = language === LANGUAGES.VI ? labelVI : labelEN;
-                object.value = item.id;
-                result.push(object);
-            })
+            if (type === 'USERS') {
+                inputData.map((item, index) => {
+                    let object = {};
+                    let labelVI = `${item.lastName} ${item.firstName}`;
+                    let labelEN = `${item.lastName} ${item.firstName}`;
+                    object.label = language === LANGUAGES.VI ? labelVI : labelEN;
+                    object.value = item.id;
+                    result.push(object);
+                })
+
+            }
+            if (type === 'PRICE') {
+                inputData.map((item, index) => {
+                    let object = {};
+                    let labelVI = `${item.valueVI}`;
+                    let labelEN = `${item.valueEN}`;
+                    object.label = language === LANGUAGES.VI ? labelVI : labelEN;
+                    object.value = item.keyMap;
+                    result.push(object);
+                })
+            }
+            if (type === 'PAYMENT') {
+                inputData.map((item, index) => {
+                    let object = {};
+                    let labelVI = `${item.valueVI}`;
+                    let labelEN = `${item.valueEN}`;
+                    object.label = language === LANGUAGES.VI ? labelVI : labelEN;
+                    object.value = item.keyMap;
+                    result.push(object);
+                })
+            }
 
         }
 
@@ -70,27 +95,33 @@ class ManageStaff extends Component {
                 listDoctor: dataSelect
             })
         }
-        if (prevProps.language !== this.props.language) {
-            let dataSelect = this.buildDataInputSelect(this.props.allDoctor)
-            this.setState({
-                listDoctor: dataSelect
-            })
-        }
+
 
         //78
 
         if (prevProps.allRequireDoctorInfor !== this.props.allRequireDoctorInfor) {
             // console.log('get data redux', this.props.allRequireDoctorInfor)
             let { resPrice, resPayment } = this.props.allRequireDoctorInfor;
-            let dataSelectPrice = this.buildDataInputSelect(resPrice)
-            let dataSelectPayment = this.buildDataInputSelect(resPayment)
-            console.log('get data new', dataSelectPrice, dataSelectPayment)
+            let dataSelectPrice = this.buildDataInputSelect(resPrice, 'PRICE')
+            let dataSelectPayment = this.buildDataInputSelect(resPayment, 'PAYMENT')
+            // console.log('get data new', dataSelectPrice, dataSelectPayment)
 
             this.setState({
                 listPrice: dataSelectPrice,
                 listPayment: dataSelectPayment
             })
 
+        }
+        if (prevProps.language !== this.props.language) {
+            let dataSelect = this.buildDataInputSelect(this.props.allDoctor, 'USERS')
+            let { resPrice, resPayment } = this.props.allRequireDoctorInfor;
+            let dataSelectPrice = this.buildDataInputSelect(resPrice, 'PRICE')
+            let dataSelectPayment = this.buildDataInputSelect(resPayment, 'PAYMENT')
+            this.setState({
+                listDoctor: dataSelect,
+                listPrice: dataSelectPrice,
+                listPayment: dataSelectPayment
+            })
         }
     }
 
@@ -102,17 +133,27 @@ class ManageStaff extends Component {
     }
 
     handleSaveContentMarkdown = () => {
+        // if()
+        let { hasOldData } = this.state;
         this.props.saveInfoDetailDoctor({
             contentHTML: this.state.contentHTML,
             contentMarkdown: this.state.contentMarkdown,
             description: this.state.description,
-            staffId: this.state.selectedOption.value
+            staffId: this.state.selectedOption.value,
+            //
+            action: hasOldData === true ? CRUD_ACTIONS.EDIT : CRUD_ACTIONS.CREATE,
+
+            selectedPrice: this.state.selectedPrice.value,
+            selectedPayment: this.state.selectedPayment.value,
+            note: this.state.note
         })
-        console.log('check state:', this.state)
+        // console.log('check state:', this.state)
 
     }
 
-    handleChangeSelect = async selectedOption => {
+
+    //ad 79
+    handleChangeSelect = async (selectedOption) => {
         // console.log('check: ',  )
         this.setState({ selectedOption });
 
@@ -124,6 +165,7 @@ class ManageStaff extends Component {
                 contentHTML: markdown.contentHTML,
                 contentMarkdown: markdown.contentMarkdown,
                 description: markdown.description,
+                // hasOldData: true
             })
         }
         else {
@@ -131,20 +173,41 @@ class ManageStaff extends Component {
                 contentHTML: '',
                 contentMarkdown: '',
                 description: '',
+                hasOldData: false
             })
 
         }
-        console.log(`check res :`, res);
+        // console.log(`check res :`, res);
     };
 
-    handleChangeDesc = (event) => {
+    //79
+    handleChangeSelectDoctorInfo = async (selectedOption, name) => {
+        //name.name lấy tên của react select
+        let stateName = name.name;
+        let stateCopy = { ...this.state }
+        stateCopy[stateName] = selectedOption;
+
         this.setState({
-            description: event.target.value
+            ...stateCopy
+        })
+        // console.log('check name: ', selectedOption, stateName)
+
+
+    }
+
+    handleChangeText = (event, id) => {
+        let stateCopy = { ...this.state }
+        stateCopy[id] = event.target.value;
+
+        this.setState({
+            ...stateCopy
         })
     }
 
     render() {
-        // console.log('check state', this.state)
+        //
+        let { hasOldData } = this.state
+        console.log('check state', this.state)
         return (
             <div className='manage-staff-container mb-5'>
                 <div className='title mt-4 '>
@@ -158,6 +221,9 @@ class ManageStaff extends Component {
                             onChange={this.handleChangeSelect}
                             options={this.state.listDoctor}
                             placeholder={'Chọn bác sĩ'}
+                            //79
+                            name={'selectedOption'}
+
                         />
 
                     </div>
@@ -165,7 +231,7 @@ class ManageStaff extends Component {
                         <label className='text-up'>Thông tin giới thiệu</label>
                         <textarea className='form-control' rows='4'
                             value={this.state.description}
-                            // onChange={(event) => this.handleChangeDesc(event)}
+                            onChange={(event) => this.handleChangeText(event, 'description')}
                             placeholder={'Nhập thông tin vào...'}
                         >
                         </textarea>
@@ -176,23 +242,32 @@ class ManageStaff extends Component {
                     <div className='col-3 form-group ms-5'>
                         <label>Chọn giá</label>
                         <Select
-                            // value={this.state.selectedOption}
-                            // onChange={this.handleChangeSelect}
+                            //79
+                            value={this.state.selectedPrice}
+                            onChange={this.handleChangeSelectDoctorInfo}
                             options={this.state.listPrice}
                             placeholder={'Chọn giá dịch vụ'}
-                        />                    </div>
+                            name="selectedPrice"
+
+                        />
+                    </div>
                     < div className='col-4 form-group'>
                         <label>Chọn phương thức thanh toán</label>
                         <Select
-                            // value={this.state.selectedOption}
-                            onChange={this.handleChangeSelect}
+                            value={this.state.selectedPayment}
+                            onChange={this.handleChangeSelectDoctorInfo}
                             options={this.state.listPayment}
                             placeholder={'Chọn phương thức thanh toán'}
+                            name={'selectedPayment'}
                         />
                     </div>
                     < div className='col-4 form-group'>
                         <label>Note</label>
-                        <input className='form-control'></input>
+                        <input className='form-control'
+                            onChange={(event) => this.handleChangeText(event, 'note')}
+                            value={this.state.note}
+                            placeholder={'Nhập thông tin vào...'}
+                        ></input>
 
                     </div>
 
@@ -207,7 +282,15 @@ class ManageStaff extends Component {
                 </div>
                 <button
                     onClick={() => this.handleSaveContentMarkdown()}
-                    className='save-markdown text-light font-weight-bold' type='button'>Lưu thông tin</button>
+                    className={hasOldData === true ? 'save-markdown text-light font-weight-bold' : 'save-markdown text-light font-weight-bold'}
+                    type='button'>
+                    {hasOldData === true ?
+                        <span>Cập nhật thông tin</span>
+                        :
+                        <span>Lưu thông tin</span>
+
+                    }
+                </button>
             </div >
         );
     }
